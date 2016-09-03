7f44e41152786264bd9b51daab45570e5f6d18ed
Dennis Khan <khandennis@gmail.com>
2016-09-02 22:57:40 -0400

    add trainers and pokemons controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :trainers, only: [:index, :show]
+  resources :pokemons, only: [:index, :show]
+
+  root 'trainers#index'
 end
