0bf736bb5d568f792a1e79f2dbbcb5ffc0a38fd4
Dennis Khan <khandennis@gmail.com>
2016-09-03 00:32:23 -0400

    spiral search

--- scripts/example.rb
+++ scripts/example.rb
@@ -9,15 +9,19 @@ client = Poke::API::Client.new
 # Store your location, you can also store your lat/lng directly
 # using client.store_lat_lng(lat, lng)
 # or using client.lat = 10, client.lng = 10, client.alt = 10
-client.store_location('New York')
+# client.store_location('New York')
+
+lat = 42.6733290
+lng = -71.1416420
+client.store_lat_lng(lat, lng)
 
 # Use Google auth with 'username@gmail.com', 'password', 'google'
 # Optionally set your Google Refresh token using client.refresh_token = 'my-token'
-client.login('username', 'password', 'ptc')
+client.login('velasystems.owner@gmail.com', '4321Vela', 'google')
 
 # Activate the encryption method to generate a signature
 # Where path is the path to your encrypt .so/.dll
-client.activate_signature('/path/to/encrypt/file')
+client.activate_signature('/Users/dkhan/Git/poke-stats/files/encrypt.so')
 
 # Get cells
 cell_ids = Poke::API::Helpers.get_cells(client.lat, client.lng)
